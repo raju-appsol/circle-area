fe231d0a7c439ed48033978251088d5b4115a6ae
Raj <raju.manepalli@stackular.com>
2024-12-27 19:13:50 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Please provide the radius of the circle");
+    String input=Console.ReadLine();
+int radius=Convert.ToInt32(input);
+double Area=3.14*radius*radius;
+Console.WriteLine("Area of the circle is " + Area);
